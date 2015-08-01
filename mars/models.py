b7f201a90f993b57f6aa7adddf61c1d9e0d375a5
Andres MRM <andres@inventati.org>
2015-08-01 09:26:45 -0300

    refac

--- mars/models.py
+++ mars/models.py
@@ -13,7 +13,7 @@ from extensions import db
 class User(db.Model):
     __tablename__ = 'user'
     __table_args__ = {'extend_existing': True}
-    extend_existing = True
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200), default='')
