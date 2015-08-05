123709d50b3fc18d74ebe5e855d634f69341aded
Andres MRM <andres@inventati.org>
2015-08-05 16:50:33 -0300

    now runOnShow is only for the first show

--- website/static/js/app/showsub.js
+++ website/static/js/app/showsub.js
@@ -19,43 +19,65 @@ define(['jquery', 'pubsub'], function($, pubsub) {
         $.each(['show', 'hide'], function (i, ev) {
             var el = $.fn[ev];
             $.fn[ev] = function () {
-                var r = el.apply(this, arguments);
+                console.log(ev, arguments)
+                var r = el.apply(this)
                 // Trigger "on show/hide" after showing/hiding to avoid problems
                 // initializing elements still not visible
-                this.trigger(ev);
+                if (arguments[0] && arguments[0].propagate) {
+                    console.log("propagando", arguments)
+                    this.trigger(ev, arguments)
+                } else {
+                    console.log("nao propagando", arguments)
+                }
                 return r
             };
         });
     })(jQuery);
 
 
+    // TODO: arrumar esse doc
     // Prepares a function (func) to be executed when an "event" is published,
     // but this should only happens while an element (selector) is visible.
     // "runOnShow" is used to force the function to be executed also when the
     // element becomes visible, not only when "event" is published.
-    function showSubscribe(event, selector, runOnShow, func) {
-        console.log(selector, "showSubscribed")
-
-        // var subscribed_func = function(eventNow, dataNow) {
-        //     console.log("eventoNow", eventNow)
-        //     func(eventNow, dataNow)
-        // }
+    function showSubscribe(event, selector, runOnFirstShow, func) {
+        console.log("showSubscribed", event, selector, runOnFirstShow, func)
+        var element = $(selector)
 
         // Starts to update automaticaly while visible
-        $(selector).on("show", function() {
-            console.log("SubSHOW", event, selector, func)
-            // pubsub.unsubscribe(subscribed_func)
+        element.on("show", function(ev) {
+            console.log("subshow", event, selector, func)
             pubsub.subscribe(event, func)
-            if (runOnShow) console.log("RUN-On-Show: ", event, selector, func)
-            if (runOnShow) func()
+            // if (runOnShow) func()
+            if (runOnFirstShow && !func.notFirstShow) {
+                func()
+            }
+            func.notFirstShow = true
         })
 
         // Stops to update automaticaly while hidden
-        $(selector).on("hide", function() {
-            console.log("SubHIDE", event, selector, func)
+        element.on("hide", function(ev) {
+            console.log("subhide", event, selector, func)
             pubsub.unsubscribe(func)
+        })
+
+        // Element is already visible, so force run
+        if (element.css('display') != 'none'){
+            console.log("already shown, force func")
+            func()
+        }
+    }
+
+    // Run 'func' the first time 'selector' is shown
+    function runOnFirstShow(selector, func) {
+        var element = $(selector)
+        element.on("show", function(ev) {
+            func()
+            element.on("show", null)
         })
     }
 
+    showSubscribe.runOnFirstShow = runOnFirstShow
+
     return showSubscribe
-});
+})
