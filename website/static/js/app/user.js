94305b0960cb2386cff4fedfa9207f9619deb7e7
Andres MRM <andres@inventati.org>
2015-08-06 12:27:35 -0300

    edit user info

--- website/static/js/app/user.js
+++ website/static/js/app/user.js
@@ -4,16 +4,54 @@ define(["jquery", 'app/urlmanager', 'showutils', 'app/templates', 'app/auth'], f
 
     var containerId = "#user-container",
         userContainer = $(containerId),
-        userTemplate = null
+        userTemplate = null,
+        username = null
 
     function initUserInterface() {
         userTemplate = templates.get("user")
     }
 
-    function updateUser(event, data) {
+    function activateButton() {
+        $("#descr-form-update-button").click(function(e) {
+            // TODO: verificar se está logado?
+            auth.validateMicroTokenTime(sendInfo)
+            return false
+        })
+    }
+
+    // Update user info
+    function sendInfo() {
+        var url = AUTH_API_URL + '/users/' + username + '/edit'
+        var data = {
+            'token': localStorage.microToken,
+            'description': $("#user-form-textarea").val(),
+        }
+        console.log(data)
+        $.ajax({
+            url        : url,
+            dataType   : 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data       : JSON.stringify(data),
+            type       : 'PUT',
+        })
+            .done(function(data) {
+                console.log("DONE", data)
+                userContainer.html(userTemplate(data))
+                activateButton()
+            })
+            .fail(function(data, error, errorName) {
+                alert(data.responseJSON.message)
+            })
+    }
+
+    function updateDisplayedInfo(event, data) {
         if (!userTemplate) initUserInterface()
         console.log("!!!!!!!!!!!!!UPADETE-USER", event, data, urlManager.getParam('username'))
-        var username = typeof data !== 'undefined' ? data.value : urlManager.getParam('username')
+        username = typeof data !== 'undefined' ? data.value : urlManager.getParam('username')
+        var ownProfile = username == auth.getUsername()
+        console.log(ownProfile, username, auth.getUsername())
+
+        // TODO: ver se é o próprio user para pegar o e-mail também?
         $.getJSON(
             AUTH_API_URL + '/users/' + username
         )
@@ -21,7 +59,9 @@ define(["jquery", 'app/urlmanager', 'showutils', 'app/templates', 'app/auth'], f
                 // TODO: caso seja a página do próprio usuário, deixar alterar dados
                 console.log("AJAX - USER", data)
                 if (!data.description) data.description = "Sem descrição..."
+                data.possibleEdit = ownProfile
                 userContainer.html(userTemplate(data))
+                activateButton()
             })
             .fail(function(data, error, errorName) {
                 // TODO: Melhorar isso
@@ -31,5 +71,5 @@ define(["jquery", 'app/urlmanager', 'showutils', 'app/templates', 'app/auth'], f
             })
     }
 
-    showutils.showSubscribe("username.changed", containerId, true, updateUser)
+    showutils.showSubscribe("username.changed", containerId, true, updateDisplayedInfo)
 })
