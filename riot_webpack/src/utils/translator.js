b5cd6f565972a16fce35a3a4795f6cec7a58f739
Andres MRM <andres@inventati.org>
2015-09-08 12:54:17 -0300

    translation

--- riot_webpack/src/utils/translator.js
+++ riot_webpack/src/utils/translator.js
@@ -1,20 +1,62 @@
+import router from '../store/router'
+
+
 class Translator {
     constructor() {
-        this.pt = {"Login": "Entrar"}
-        this.en = {"Login": "LOGIN!!!!"}
-        this.dict = this.pt
+        this.dicts = {}
+
+        riot.control.on(riot.SEC('lang'), (lang) => {
+            this.setLang(lang)
+        })
+        // this._currentLang = this.getCurrentLang()
+        this._currentLang = router.getParam('lang')
+        console.log('translator: initial lang:', this._currentLang)
     }
 
-    translate(text) {
-        return text
+    init() {
+        this.dicts['pt-br'] = {
+            '_lang': 'Português',
+            'Share': 'Compartilhar',
+            'Logout': 'Sair',
+            'About': 'Sobre',
+            'Another': 'Outra',
+            'Login': 'Entrar',
+            'Register': 'Registrar',
+            'Locate': 'Localizar',
+            'Year': 'Ano',
+        }
+
+        this.dicts.en = {
+            '_lang': 'English',
+        }
     }
 
-    toggleLang() {
-        if (this.dict == this.pt) this.dict = this.en
-        else this.dict = this.pt
+    getCurrentLang() {
+        return this._currentLang
+    }
+
+    getLangs() {
+        return Object.keys(this.dicts).map(k => {
+            return {code: k, name: this.dicts[k]._lang}
+        })
+    }
+
+    setLang(lang) {
+        if(this._currentLang != lang) {
+            this._currentLang = lang
+            riot.update()
+        }
+    }
+
+    translate(str) {
+        console.log(`translator:translate: ${str} using:`, this._currentLang)
+        let translated = this.dicts[this._currentLang][str]
+        console.log(`resulted: ${translated}`)
+        return translated ? translated : str
     }
 }
 
 let translator = new Translator()
+translator.init()
 
 export default translator
