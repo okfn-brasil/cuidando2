9591dd8841f11262abf5e9490297f58d80f5785c
Andres MRM <andres@inventati.org>
2015-09-10 10:20:54 -0300

    donut & bar "proto" charts

---
+++ riot_webpack/src/component/chart/chart-utils.js
@@ -0,0 +1,28 @@
+export function drawDonut(canvas, color, percentage) {
+    let context = canvas.getContext('2d'),
+        x = canvas.width / 2,
+        y = canvas.height / 2,
+        radius = 75,
+        startAngle = 1.5 * Math.PI,
+        endAngle = (1.5 + 2 * percentage/100) * Math.PI,
+        counterClockwise = false,
+        whiteSpace = 0.01 * Math.PI,
+        shadowStartAngle = endAngle + whiteSpace,
+        shadowEndAngle = startAngle + 2 * Math.PI - whiteSpace
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+    context.lineWidth = 15;
+
+    // line color
+    context.strokeStyle = color
+    context.stroke()
+
+    if (shadowStartAngle < shadowEndAngle) {
+        context.beginPath();
+        context.arc(x, y, radius, shadowStartAngle, shadowEndAngle, counterClockwise);
+        context.strokeStyle = 'lightgray';
+        context.stroke();
+    }
+}
--- riot_webpack/src/riotTags.js
+++ riot_webpack/src/riotTags.js
@@ -1,8 +1,9 @@
 import './component/menubar.html'
 import './component/yearselector.html'
 import './component/map.html'
-import './component/charts.html'
+import './component/chart/charts.html'
 import './component/chart/donut.html'
+import './component/chart/bar.html'
 
 import './view/texto-view.html'
 import './view/ano-view.html'
