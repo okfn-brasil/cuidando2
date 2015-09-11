16500bd8a0325e74e162082dfe0164997cc73b30
Andres MRM <andres@inventati.org>
2015-09-11 15:03:42 -0300

    polyfills for older browsers

--- riot_webpack/src/vendor.js
+++ riot_webpack/src/vendor.js
@@ -1,4 +1,14 @@
-// import './css/tacit.min.css'
+// TODO: Remover ou otimizar load abaixo
+// import 'core-js'
+// import 'babel/polyfill'
+// import 'es6-symbol/implement'
+import 'core-js/fn/symbol'
+import 'core-js/fn/array/iterator'
+import 'core-js/fn/object/assign'
+import 'core-js/fn/promise.js'
+// import 'es6.array.iterator'
+// import 'core-js/fn/symbol/iterator'
+
 import 'riot'
 import 'leaflet'
 import 'leaflet.markercluster'
