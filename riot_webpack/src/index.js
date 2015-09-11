fe53a737b62cd49f7d2cd4928ef14a76e578a5c0
Andres MRM <andres@inventati.org>
2015-09-11 15:04:46 -0300

    auth

--- riot_webpack/src/index.js
+++ riot_webpack/src/index.js
@@ -1,7 +1,7 @@
 import './RiotControl.js'
+import './stores.js'
 import './map/map.js'
 import './utils/basemixin.js'
-import './stores.js'
 import './riotTags.js'
 
 import './css/app.sass'
--- riot_webpack/src/store/auth.js
+++ riot_webpack/src/store/auth.js
@@ -2,12 +2,57 @@ import decodeToken from 'jwt-decode'
 import ajax from '../utils/ajax2'
 import config from '../config.js'
 
+let hasLocalStorage = true
+
+// Test localStorage
+// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
+try {
+    localStorage.setItem('t', 't')
+    localStorage.removeItem('t')
+} catch (e) {
+    hasLocalStorage = false
+    alert('Seu navegador parece não suportar localStorage. Por favor use um mais recente, ou você não conseguirá autenticar nesse site...')
+}
+
+// This code is needed to restore the hash after a redirect for login
+// through a third-party.
+// If redicected for login (from Facebook)
+if (/^\?redirect_state=/.test(location.search)) {
+    var url = location.origin + "/" + localStorage.prevhash
+    // Save info to get tokens after page load/reload
+    // If History is supported, tokens should be loaded after page load.
+    // If History isn't supported, tokens should be loaded after page reload.
+    localStorage.queryForToken = location.search
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, url)
+    } else {
+        // Fallback method that doesn't requires "window.history" but
+        // reloads the page.
+        location.href = url
+    }
+}
+
+
 class Auth {
     constructor(signal) {
         riot.observable(this)
 
+        // The page was reloaded and a getToken is pending.
+        // Needed when browser has no support for window.history
+        if (localStorage.queryForToken) {
+            this.completeFacebook(localStorage.queryForToken)
+            localStorage.queryForToken = ""
+        }
+
         console.log('register-event')
         this.on(riot.VEC('register'), params => this.register(params))
+        this.on(riot.VEC('login'), params => this.login(params))
+        this.on(riot.VEC('loginFacebook'), () => this.loginFacebook())
+        this.on(riot.VEC('logout'), () => this.logout())
+        // View requesting current username
+        this.on(riot.VEL('username'), () => {
+            this.trigger(riot.SEC('username'), this.getUsername())
+        })
         riot.control.addStore(this)
     }
 
@@ -50,6 +95,51 @@ class Auth {
             data: {password: params.password, email: params.email},
             method: 'post',
         }).then(this.saveTokens.bind(this))
+    }
+
+    login(params) {
+        ajax({
+            url: config.apiurl_auth + "/login/local",
+            data: {username: params.username, password: params.password},
+            method: 'post',
+        }).then(this.saveTokens.bind(this))
+    }
+
+    loginFacebook() {
+        ajax({
+            url: config.apiurl_auth + '/login/external/manual/facebook',
+            method: 'get',
+        }).then(response => {
+            let origRedirect = response.redirect,
+                thisUrl = window.location.origin,
+                parts = origRedirect.split(escape("?")),
+                newRedirect = parts[0]
+                    .replace(/(redirect_uri=)[^\&]+/, '$1' + thisUrl) +
+                    escape("?") + parts[1]
+            localStorage.prevhash = location.hash
+            // redirect to site for login
+            console.log("NEW-REDIRECT:", newRedirect)
+            location.href = newRedirect
+        })
+    }
+
+    // Complete Facebook login after redirect
+    completeFacebook(query) {
+        ajax({
+            url: config.apiurl_auth + "/complete/manual/facebook" + query,
+            method: 'post',
+        }).then(this.saveTokens.bind(this))
+    }
+
+    logout() {
+        ajax({
+            url: config.apiurl_auth + "/logout",
+            data: {'token': localStorage.mainToken},
+            method: 'post',
+        })
+        localStorage.removeItem("mainToken")
+        localStorage.removeItem("microToken")
+        this.trigger(riot.SEC('username'), null)
     }
 }
 
