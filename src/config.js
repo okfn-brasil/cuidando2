10eaea3ca4857f964b6183c2b604e5df46f6424c
Andres MRM <andres@inventati.org>
2015-09-15 11:00:12 -0300

    prod/dev configs

--- src/config.js
+++ src/config.js
@@ -1,16 +1,15 @@
+// const _domain = 'http://cuidando.org.br:'
+// const config = {
+//     apiurl_money: 'http://demo.gastosabertos.org',
+//     apiurl_auth: _domain + 5002,
+//     apiurl_comments: _domain + 5003,
+// }
+
 const _domain = 'http://cuidando.aqui:'
-const _domain_prod = 'http://cuidando.org.br:'
-
-// API_URL = _domain % '5000'
-// AUTH_API_URL = _domain % '5002'
-// COMMENTS_API_URL = _domain % '5003'
-// ESIC_API_URL = _domain % '5004'
-
 const config = {
     apiurl_money: _domain + 5000,
     apiurl_auth: _domain + 5002,
     apiurl_comments: _domain + 5003,
-    // apiurl_comments: _domain_prod + 5003,
 }
 
 export default config
