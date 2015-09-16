722845a02768976748c6236d05899930362ab8fc
Andres MRM <andres@inventati.org>
2015-09-16 14:36:59 -0300

    table styling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,16 @@
 import router from '../store/router'
 
 // Convert number to locale string
-export function format(number) {
-    return number.toLocaleString(router.getParam('lang'))
+export function format(number, opts) {
+    return number.toLocaleString(router.getParam('lang'), opts)
+}
+
+// Convert number to locale string with 2 decimal digits
+export function formatCur(number) {
+    return format(number, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2}
+    )
 }
 
 // Capitalize first letter
