bb380261e38bbb1cd133add9599edf43cb05a856
Andres MRM <andres@inventati.org>
2015-09-28 00:27:22 -0300

    refac

--- configs/config.js
+++ configs/config.js
@@ -1,8 +1,10 @@
 const _domain = 'http://cuidando.aqui:'
 const config = {
-    apiurl_money: _domain + 5000,
+    apiurl_money: 'http://demo.gastosabertos.org',
+    // apiurl_money: _domain + 5000,
     apiurl_auth: _domain + 5002,
     apiurl_comments: _domain + 5003,
+    apiurl_esic: _domain + 5004,
 }
 
 export default config
