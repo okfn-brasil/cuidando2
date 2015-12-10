9c77ec6ca768eaf8eb88945f21d87d5ba695cdf2
Andres MRM <andres@inventati.org>
2015-12-10 08:50:31 -0200

    addrs

--- configs/config.js
+++ configs/config.js
@@ -1,10 +1,13 @@
 const _domain = 'http://cuidando.aqui:'
 const config = {
-    apiurl_money: 'http://demo.gastosabertos.org',
+    apiurl_money: 'https://site-cuidando.rhcloud.com/dados/api/v1/',
     // apiurl_money: _domain + 5000,
-    apiurl_auth: _domain + 5002,
-    apiurl_comments: _domain + 5003,
-    apiurl_esic: _domain + 5004,
+    apiurl_auth: 'https://viralata-cuidando.rhcloud.com',
+    // apiurl_auth: _domain + 5002,
+    apiurl_comments: 'https://tagarela-cuidando.rhcloud.com',
+    // apiurl_comments: _domain + 5003,
+    apiurl_esic: 'https://cuidando.vc/esiclivre',
+    // apiurl_esic: _domain + 5004,
 }
 
 export default config
